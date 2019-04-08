416a949f46f0deded2a3673085d93364853ac9c5
Venkat Garapati <venkat_garapati@outlook.com>
2019-04-08 18:32:30 -0400

    Adjust Board Connection Code.

--- main.cpp
+++ main.cpp
@@ -1,105 +1,110 @@
-#include "mbed.h"
 #include "WIZnetInterface.h"
-#define ECHO_SERVER_PORT   80
- 
-SPI spi(PTD7, PTD6, PTD5); // mosi, miso, sclk
-WIZnetInterface eth(&spi, PTD4, PTA20); // spi, cs, reset
-Serial pc(PTA2,PTA1);
-DigitalOut led(PTB0);
- 
+#include "mbed.h"
+
+#define ECHO_SERVER_PORT 80
+
+Serial pc(PTA2, PTA1);
+SPI spi(D12, D11, D13);                // mosi, miso, sclk
+WIZnetInterface eth(&spi, D10, D2); // spi, cs, reset
+
+DigitalOut ledBlue(LED_BLUE, 1);
+DigitalOut ledRed(LED_RED, 1);
+DigitalOut ledGreen(LED_GREEN, 1);
+
 void f_ethernet_init(void);
- 
- 
-const char * IP_Addr    = "169.254.175.210";
-const char * IP_Subnet  = "255.255.255.0";
-const char * IP_Gateway = "169.254.175.209";
-int ret,dummy;
-int lv=1;
- 
- 
-char a[]={"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nPragma: no-cache\r\n\r\n"};
-char b[]={"<html>\r\n<body>\r\n"};
-char c[]={"<title>FRDM KL46Z</title>\r\n"};
-char d[]={"<h2>Web using Wiznet </h2>\r\n"};
-char e[]={"<br /><hr>\r\n"};
-char f[]={"<FONT FACE=\"Arial\" SIZE=\"2\" COLOR=\"FF0000\">"};
- 
+
+const char *IP_Addr = "169.254.175.210";
+const char *IP_Subnet = "255.255.255.0";
+const char *IP_Gateway = "169.254.175.1";
+uint8_t mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+int ret, dummy;
+int lv = 1;
+
+char a[] = {
+    "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nPragma: no-cache\r\n\r\n"};
+char b[] = {"<html>\r\n<body>\r\n"};
+char c[] = {"<title>FRDM KL46Z</title>\r\n"};
+char d[] = {"<h2>Web using Wiznet </h2>\r\n"};
+char e[] = {"<br /><hr>\r\n"};
+char f[] = {"<FONT FACE=\"Arial\" SIZE=\"2\" COLOR=\"FF0000\">"};
+
 char paq_en[32];
- 
- 
- 
-int main()
-{
-    f_ethernet_init();
-    TCPSocketServer server;
-    server.bind(ECHO_SERVER_PORT);
-    server.listen();
-    pc.printf("\nWaiting for client...\n");
-    TCPSocketConnection client;
-    while(lv)
-    {
-        lv=server.accept(client);
-        if(!lv)
-        {
-           pc.printf("Client connected: %s\n", client.get_address());
-          
-        }
-        else
-        {
-           wait(10);
-        }
-    }   
-    
- 
-           client.send(a,sizeof(a));
-           client.send(b,sizeof(b));
-           client.send(c,sizeof(c));
-           client.send(d,sizeof(d));
-           client.send(e,sizeof(e));
-           client.send(f,sizeof(f));
-        
-              
-    
-    
-    while(1)
-    {
-      dummy++;
-      sprintf(paq_en,"<FONT><br>Contador=%d</FONT>",dummy); 
-      client.send(paq_en,sizeof(paq_en));
-      wait(1);
-    } 
-}   
- 
-void f_ethernet_init()
-{
-     uint8_t mac[]={0x00,0x08,0xDC,0x1C,0xAA,0xCA};
-   // mbed_mac_address((char *)mac); 
-    pc.printf("\tInitialize Ethernet...\n\r");
-    wait(1);
-    ret = eth.init(mac, IP_Addr, IP_Subnet, IP_Gateway);
-    if(!ret)
-    {
-        pc.printf("MAC= %s\n\r",eth.getMACAddress());
-    }    
-    else
-    {
-        pc.printf("Failed to set up ethernet...\n\r");    
+
+void toggleRed() { ledRed = !ledRed; }
+
+void toggleGreen() { ledGreen = !ledGreen; }
+
+void toggleBlue() { ledBlue = !ledBlue; }
+
+int main() {
+  f_ethernet_init();
+
+  TCPSocketServer server;
+  server.bind(ECHO_SERVER_PORT);
+  server.listen();
+  pc.printf("\nWaiting for client...\n");
+
+  TCPSocketConnection client;
+  toggleBlue();
+  while (lv) {
+
+    lv = server.accept(client);
+    if (!lv) {
+      pc.printf("Client connected: %s\n", client.get_address());
+      toggleBlue();
+      toggleGreen();
+
+    } else {
+      toggleBlue();
+      toggleRed();
+      wait(10);
     }
-    pc.printf("Connected.");
-    wait(2);
-    pc.printf(".");
-    wait(2);
-    pc.printf(".\n\r");
+  }
+
+  client.send(a, sizeof(a));
+  client.send(b, sizeof(b));
+  client.send(c, sizeof(c));
+  client.send(d, sizeof(d));
+  client.send(e, sizeof(e));
+  client.send(f, sizeof(f));
+
+  while (1) {
+    dummy++;
+    sprintf(paq_en, "<FONT><br>Contador=%d</FONT>", dummy);
+    client.send(paq_en, sizeof(paq_en));
     wait(1);
-    ret = eth.connect();
-    if(!ret)
-    {
-        pc.printf("Connection Establisheds!\n\n\r");
-        wait(1);
-        pc.printf("IP=%s\n\rMASK=%s\n\rGW=%s\n\r",eth.getIPAddress(), eth.getNetworkMask(), eth.getGateway());
-    }    
-    else
-    {
-        pc.printf("Failed Connection\n\r"); 
-    }
-}  
+  }
+}
+
+void f_ethernet_init() {
+
+  // mbed_mac_address((char *)mac);
+  pc.printf("\tInitialize Ethernet...\n\r");
+  wait(1);
+  pc.printf("Expected MAC= %02X:%02X:%02X:%02X:%02X:%02X\n\r", mac[0], mac[1],
+            mac[2], mac[3], mac[4], mac[5]);
+  pc.printf("Before MAC= %s\n\r", eth.getMACAddress());
+  ret = eth.init(mac, IP_Addr, IP_Subnet, IP_Gateway);
+  if (!ret) {
+    pc.printf("After MAC= %s\n\r", eth.getMACAddress());
+  } else {
+    pc.printf("Failed to set up ethernet...\n\r");
+    toggleRed();
+  }
+  pc.printf("Board Connected.");
+  wait(2);
+  pc.printf(".");
+  wait(2);
+  pc.printf(".\n\r");
+  wait(1);
+  ret = eth.connect();
+  if (!ret) {
+    pc.printf("Board Connection Established!\n\n\r");
+    wait(1);
+    pc.printf("IP=%s\n\rMASK=%s\n\rGW=%s\n\r", eth.getIPAddress(),
+              eth.getNetworkMask(), eth.getGateway());
+  } else {
+    pc.printf("Failed Connection\n\r");
+    toggleRed();
+  }
+}
---
+++ mbed_config.h
@@ -0,0 +1,77 @@
+/*
+ * mbed SDK
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Automatically generated configuration file.
+// DO NOT EDIT, content will be overwritten.
+
+#ifndef __MBED_CONFIG_DATA__
+#define __MBED_CONFIG_DATA__
+
+// Configuration parameters
+#define MBED_CONF_ATMEL_RF_ASSUME_SPACED_SPI                 0                                                                              // set by library:atmel-rf
+#define MBED_CONF_ATMEL_RF_FULL_SPI_SPEED                    7500000                                                                        // set by library:atmel-rf
+#define MBED_CONF_ATMEL_RF_FULL_SPI_SPEED_BYTE_SPACING       250                                                                            // set by library:atmel-rf
+#define MBED_CONF_ATMEL_RF_IRQ_THREAD_STACK_SIZE             1024                                                                           // set by library:atmel-rf
+#define MBED_CONF_ATMEL_RF_LOW_SPI_SPEED                     3750000                                                                        // set by library:atmel-rf
+#define MBED_CONF_ATMEL_RF_PROVIDE_DEFAULT                   0                                                                              // set by library:atmel-rf
+#define MBED_CONF_ATMEL_RF_USE_SPI_SPACING_API               0                                                                              // set by library:atmel-rf
+#define MBED_CONF_DRIVERS_UART_SERIAL_RXBUF_SIZE             256                                                                            // set by library:drivers
+#define MBED_CONF_DRIVERS_UART_SERIAL_TXBUF_SIZE             256                                                                            // set by library:drivers
+#define MBED_CONF_ESP8266_DEBUG                              0                                                                              // set by library:esp8266
+#define MBED_CONF_ESP8266_PROVIDE_DEFAULT                    0                                                                              // set by library:esp8266
+#define MBED_CONF_ESP8266_SOCKET_BUFSIZE                     8192                                                                           // set by library:esp8266
+#define MBED_CONF_EVENTS_PRESENT                             1                                                                              // set by library:events
+#define MBED_CONF_EVENTS_SHARED_DISPATCH_FROM_APPLICATION    0                                                                              // set by library:events
+#define MBED_CONF_EVENTS_SHARED_EVENTSIZE                    256                                                                            // set by library:events
+#define MBED_CONF_EVENTS_SHARED_HIGHPRIO_EVENTSIZE           256                                                                            // set by library:events
+#define MBED_CONF_EVENTS_SHARED_HIGHPRIO_STACKSIZE           1024                                                                           // set by library:events
+#define MBED_CONF_EVENTS_SHARED_STACKSIZE                    2048                                                                           // set by library:events
+#define MBED_CONF_EVENTS_USE_LOWPOWER_TIMER_TICKER           0                                                                              // set by library:events
+#define MBED_CONF_MCR20A_PROVIDE_DEFAULT                     0                                                                              // set by library:mcr20a
+#define MBED_CONF_PLATFORM_CRASH_CAPTURE_ENABLED             0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_CTHUNK_COUNT_MAX                  8                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE          9600                                                                           // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_ALL_THREADS_INFO            0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_DECODE_HTTP_URL_STR         "\nFor more info, visit: https://armmbed.github.io/mbedos-error/?error=0x%08X" // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_FILENAME_CAPTURE_ENABLED    0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_HIST_ENABLED                0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_HIST_SIZE                   4                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_REBOOT_MAX                  1                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_FATAL_ERROR_AUTO_REBOOT_ENABLED   0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_FORCE_NON_COPYABLE_ERROR          0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_MAX_ERROR_FILENAME_LEN            16                                                                             // set by library:platform
+#define MBED_CONF_PLATFORM_POLL_USE_LOWPOWER_TIMER           0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_BAUD_RATE                   9600                                                                           // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_BUFFERED_SERIAL             0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES            0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_CONVERT_TTY_NEWLINES        0                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_FLUSH_AT_EXIT               1                                                                              // set by library:platform
+#define MBED_CONF_PLATFORM_USE_MPU                           1                                                                              // set by library:platform
+#define MBED_CONF_RTOS_IDLE_THREAD_STACK_SIZE                512                                                                            // set by library:rtos
+#define MBED_CONF_RTOS_IDLE_THREAD_STACK_SIZE_TICKLESS_EXTRA 256                                                                            // set by library:rtos
+#define MBED_CONF_RTOS_MAIN_THREAD_STACK_SIZE                4096                                                                           // set by library:rtos
+#define MBED_CONF_RTOS_PRESENT                               1                                                                              // set by library:rtos
+#define MBED_CONF_RTOS_THREAD_STACK_SIZE                     4096                                                                           // set by library:rtos
+#define MBED_CONF_RTOS_TIMER_THREAD_STACK_SIZE               768                                                                            // set by library:rtos
+#define MBED_CONF_S2LP_PROVIDE_DEFAULT                       0                                                                              // set by library:s2lp
+#define MBED_CONF_TARGET_BOOT_STACK_SIZE                     0x400                                                                          // set by library:rtos[*]
+#define MBED_CONF_TARGET_DEEP_SLEEP_LATENCY                  0                                                                              // set by target:Target
+#define MBED_CONF_TARGET_MPU_ROM_END                         0x0fffffff                                                                     // set by target:Target
+// Macros
+#define _RTE_                                                                                                                               // defined by library:rtos
+
+#endif
